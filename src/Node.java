7164bf1e9915873a9aa8862fbb053301f28ca4f6
Yarin Katan <yarin.katan@campus.technion.ac.il>
2023-05-21 12:46:14 +0300

    א

--- src/Node.java
+++ src/Node.java
@@ -72,6 +72,51 @@ public class Node {
      * @return the heuristic value.
      */
     public int heuristicValue() {
+      int h1 = calculateHeuristic1();
+      int h2 = calculateHeuristic2();
+      return (int)(0.1 * h1 + 0.9 * h2);
+    }
+
+    /**
+     * Calculates the heuristic value for the sliding puzzle game
+     * find the first option for the heuristic value
+     * @return The heuristic value for the current state of the sliding puzzle game
+     */
+    public int calculateHeuristic1() {
+        Tile[][] size = state.getBoard().getTiles();
+        int heuristic = 0;
+
+        // Calculate a distance measure that is calculated by taking the sum of distances between the x and y coordinates
+        for (int i = 0; i < size.length; i++) {
+            for (int j = 0; j < size[i].length; j++) {
+                int value = size[i][j].getValue();
+                if (value != 0) {  // Ignore the empty tile
+                    int targetRow = (value - 1) / size.length;
+                    int targetCol = (value - 1) % size.length;
+                    // Calculate the distance between the current position and the target position
+                    if((i - targetRow)<0) {
+                        heuristic += -1*(i - targetRow);
+                    }else{
+                        heuristic += (i - targetRow);
+                    }
+                    if ((j - targetCol)<0){
+                        heuristic += -1*(j - targetCol);
+                    }else{
+                        heuristic += (j - targetCol);
+                    }
+                }
+            }
+        }
+
+        return heuristic;
+    }
+
+    /**
+     * Calculates the heuristic value for the sliding puzzle game
+     * find the second option for heuristic value
+     * @return the board with the least tiles that are not in place
+     */
+    public int calculateHeuristic2(){
         if(this.state.isGoal()) {
             return 0; /* Sets the heuristic value to 0 if the node's state is the goal state. */
         }
@@ -91,3 +136,4 @@ public class Node {
         return count;
     }
 }
+
